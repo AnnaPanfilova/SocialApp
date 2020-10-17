02ec45a756d10e96581f931bda5ab4556049f431
Anna <anna.panfilova2107@gmail.com>
2020-10-17 13:17:16 +0300

    ДЗ 4 - Лайки + Индексная таблица ДЗ 5 - Использование Touch ID / Face ID

--- SocialApp/PhotoCollectionViewCell.swift
+++ SocialApp/PhotoCollectionViewCell.swift
@@ -11,7 +11,23 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
     
-    var likes = 0;
+    var likes = 0 {
+        didSet {
+            likeButton.setTitle(String(likes), for: .normal)
+            
+            var color = UIColor.darkGray
+            var image = UIImage(systemName: "heart")
+            
+            if likes > 0 {
+                color = UIColor.red
+                image = UIImage(systemName: "heart.fill")
+            }
+
+            likeButton.tintColor = color
+            likeButton.setTitleColor(color, for: .normal)
+            likeButton.setImage(image, for: .normal)
+        }
+    }
     
     @IBAction func likeButtonAction(_ sender: Any) {
         if likes == 0 {
@@ -19,8 +35,25 @@ class PhotoCollectionViewCell: UICollectionViewCell {
         } else {
             likes -= 1
         }
+    
+    }
+    
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        if super.point(inside: point, with: event) {
+            return true
+        }
         
-        likeButton.imageView?.image = UIImage(named: likes > 0 ? "heart.fill" : "heart")
+        return likeButton.point(inside: likeButton.convert(point, to: self), with: event)
+    }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        var view = likeButton.hitTest(likeButton.convert(point, to: self), with: event)
+        
+        if view == nil {
+            view = super.hitTest(point, with: event)
+        }
+        
+        return view
     }
     
 }
